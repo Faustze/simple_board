17d549b6d2bbb8cd2479260f50c9013c0aa452e6
Artem Suvorov <baros24@mail.ru>
2025-03-22 12:17:56 +0300

    Add create post and view posts options

--- board/posts.py
+++ board/posts.py
@@ -1,3 +1,5 @@
+import sqlalchemy as sa
+
 from flask import (
     Blueprint, 
     render_template, 
@@ -6,6 +8,7 @@ from flask import (
     url_for,
 )
 from board.extensions import db
+from board.models import Post
 
 
 bp = Blueprint('posts', __name__)
@@ -13,14 +16,19 @@ bp = Blueprint('posts', __name__)
 @bp.route('/create', methods=['GET', 'POST'])
 def create():
     if request.method == 'POST':
-        author = request.form['author'] or 'Anonymous'
-        message = request.form['message']
+        author = request.form.get('author','Anonymous')
+        message = request.form.get('message')
 
         if message:
-            pass
+            new_post = Post(author=author, message=message)
+            db.session.add(new_post)
+            db.session.commit()
+            return redirect(url_for('posts.posts'))
+        
     return render_template('posts/create.html')
 
 @bp.route('/posts')
 def posts():
-    posts = []
+    query = sa.select(Post).order_by(Post.created.desc())
+    posts = db.session.scalars(query).all()
     return render_template('posts/posts.html', posts=posts)
